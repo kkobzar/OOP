c237ff8b2777a48737f15553b0a778efbe4de9d3
Kiril Kobzar <kirilkobzar@gmail.com>
2022-12-13 18:03:48 +0200

    bubble sort

--- task_2/task_1.cpp
+++ task_2/task_1.cpp
@@ -1,6 +1,11 @@
 //Kyryl Kobzar
 //Variant 6
+
+//CustomArray::bubbleSort(): 
 //Розробити метод-член класу впорядкування по зростанню методом бульбашки одновимірного динамічного масиву.
+
+//Розробити метод-член класу для створення нового одновимірного масиву 
+//з кількості всіх негативних елементів кожного рядка заданого двовимірного динамічного масиву.
 
 #include <stdlib.h>
 #include <iostream>
@@ -39,10 +44,24 @@ public:
 
 	void OutputArr()
 	{
-		cout << "Array" << endl;
+		cout << "Array:" << endl;
 		for (int i = 0; i < size; i++)
 		{
 			cout << "["<<i<<"]: " << array[i] << endl;
+		}
+	};
+
+	void bubbleSort()
+	{
+		for (int i = 0; i < size; i++)
+		{
+			for (int j = 0; j < size - i; j++)
+			{
+				if (array[j] > array[j+1])
+				{
+					swap(array[j],array[j+1]);
+				}
+			}
 		}
 	};
 	
@@ -54,6 +73,9 @@ int main(int argc, char const *argv[])
 	CustomArray arr(8);
 
 	arr.InputArr();
+	arr.OutputArr();
+	arr.bubbleSort();
+	cout << "******"<<endl << "Array after sort:" << endl << "******" << endl;
 	arr.OutputArr();
 	return 0;
 }
