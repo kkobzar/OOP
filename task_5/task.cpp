a7d7d6eb3a9612ab58c3addc1a0d5db0ebc15b54
Kiril Kobzar <kirilkobzar@gmail.com>
2022-12-15 15:19:13 +0200

    find for string occurs

--- task_5/task.cpp
+++ task_5/task.cpp
@@ -9,3 +9,46 @@
 
 using namespace std;
 
+class Counter
+{
+protected:
+	string strToCompare = "teststringuuuUUU12341234";
+public:
+	virtual int CountRepeats(string comp) = 0;
+	
+};
+
+class RepeatCounter :public Counter
+{
+public:
+	RepeatCounter(){};
+	~RepeatCounter(){};
+	int CountRepeats(string comp) override{
+
+		//https://stackoverflow.com/a/8614196/11526784
+
+		int occurrences = 0;
+		string::size_type start = 0;
+
+		while ((start = this->strToCompare.find(comp, start)) != string::npos) {
+		    ++occurrences;
+		    start += comp.length(); 
+		}
+
+		cout<<this->strToCompare<<endl;
+		return occurrences;
+	}
+	
+};
+
+int main(int argc, char const *argv[])
+{
+	RepeatCounter c;
+	string findFor;
+	cout << "Enter string to look for: ";
+	cin >> findFor;
+	cout << "In string: ";
+	int occ = c.CountRepeats(findFor);
+	cout << "Occurrences: " << occ <<endl;
+	return 0;
+}
